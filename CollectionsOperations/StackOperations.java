a7b1dedb892c39431bb1c5220e3927ae95ca00bb
Avetis Aghajanyan <avetisbs2@gmail.com>
2026-08-17 22:02:19 +0400

    Did collections exercises

---
+++ CollectionsOperations/StackOperations.java
@@ -0,0 +1,24 @@
+package CollectionsOperations;
+
+import java.util.Stack;
+
+public class StackOperations {
+    public static void main(String[] args) {
+        reverseString("Avetis");
+    }
+
+    // 16. Reverse a string by pushing characters onto a stack and popping them back.
+    static void reverseString(String s){
+        Stack <Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            reversed.append(stack.pop());
+        }
+        System.out.println("Reversed string: " + reversed);
+    }
+}
